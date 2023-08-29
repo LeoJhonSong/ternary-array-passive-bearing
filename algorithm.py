582e3b1cd83a581c393a8116bfd1dc94f4405beb
LeoJhon.Song <leojhon.song@outlook.com>
2023-08-29 10:56:44 +0800

    feat(算法框架): :sparkles: 添加解模糊函数

--- algorithm.py
+++ algorithm.py
@@ -2,8 +2,33 @@ import numpy as np
 from scipy import signal
 from scipy.ndimage import gaussian_filter
 
+
+def ambiguity_resolution(phi12_frac: np.ndarray, phi23_frac: np.ndarray, K: float, d: float):
+    phi43 = phi23_frac - phi12_frac
+    # 1. 修正phi43使-1/2 < phi_43 < 1/2
+    phi43 = phi43 - (phi43 > 0.5).astype(int) + (phi43 < -0.5).astype(int)
+    # 2. 由phi43正负修正phi12_frac, phi23_frac # FIXME: phi43=0?
+    sign43 = np.sign(phi43)
+    phi12_frac = phi12_frac + sign43 * (sign43 * np.sign(phi12_frac) == -1).astype(int)
+    phi23_frac = phi23_frac + sign43 * (sign43 * np.sign(phi23_frac) == -1).astype(int)
+    # 3. 解得n12, n23
+    n12 = np.zeros(phi12_frac.shape, dtype=int)
+    n23 = np.zeros(phi23_frac.shape, dtype=int)
+    for i in range(len(phi12_frac)):
+        n12[i] = np.round((phi23_frac[i]  - K * phi12_frac[i]) / (K - 1))
+        if phi43[i] > 0 and n12[i] < 0:
+            n12[i] = np.round((phi23_frac[i]  - K * phi12_frac[i] + 1) / (K - 1))
+            n23[i] = n12[i] + 1
+        elif phi43[i] < 0 and n12[i] > 0:
+            n12[i] = np.round((phi23_frac[i]  - K * phi12_frac[i] - 1) / (K - 1))
+            n23[i] = n12[i] - 1
+        else:
+            n23[i] = n12[i]
+    return n12 + phi12_frac, n23 + phi23_frac
+
+
 def far_locate(t12_f, t23_f, c, K, d):  # FIXME: 还是模糊相位角?
-    """远场条件下的定位解算
+    """远场条件下的位置解算
 
     Parameters
     ----------
--- simulation/signal.py
+++ simulation/signal.py
@@ -1,4 +1,5 @@
 import numpy as np
+from .perlin import generate_perlin_noise_2d
 
 
 def sig_gen(c: float, f: float, r: float, fs: float, T: float) -> np.ndarray:
@@ -23,5 +24,13 @@ def sig_gen(c: float, f: float, r: float, fs: float, T: float) -> np.ndarray:
         信号序列s[r, n]
     """
     rng = np.random.default_rng()
-    # return 1 / r * (np.cos(2 * np.pi * f * (np.arange(0, T, 1 / fs) - r / c)) + 0.01 * noise)  # TODO: 可选perlin噪声
-    return 1 / r * (np.cos(2 * np.pi * f * (np.arange(0, T, 1 / fs) - r / c)) + 0.2 * rng.standard_normal(int(T * fs)))
+
+    np.random.seed(0)  # 确保每次随机种子一样
+    noise_template = generate_perlin_noise_2d((256, 256), (8, 8))
+    noise_template = np.interp(noise_template, [np.min(noise_template), np.max(noise_template)], [-1, 1])
+    # plt.imshow(noise_template, cmap='gray', interpolation='lanczos')
+    # plt.show()
+    noise = noise_template.flatten()
+    noise = np.tile(noise, 1 + int(T * fs / (len(noise))))[:int(T * fs)]
+    return 1 / r * (np.cos(2 * np.pi * f * (np.arange(0, T, 1 / fs) - r / c)) + 0.7 * noise + 0.6 * rng.standard_normal(int(T * fs)))  # TODO: 可选perlin噪声
+    # return 1 / r * (np.cos(2 * np.pi * f * (np.arange(0, T, 1 / fs) - r / c)) + 0.4 * rng.standard_normal(int(T * fs)))
