eba57db14f384c9b5dcc5e13c48343303eb0dc6e
LeoJhon.Song <leojhon.song@outlook.com>
2023-09-14 13:02:04 +0800

    feat(算法细化): :sparkles: 添加针对chrip信号的时延估计方式: 先插值再与高分辨率参考信号比较
    顺便添加近场位置解算函数
    
    close #10

--- algorithm.py
+++ algorithm.py
@@ -1,6 +1,6 @@
 from typing import Literal, Tuple
 import numpy as np
-from scipy import signal
+from scipy import signal, interpolate
 
 
 def ambiguity_resolution(phi12_frac: float, phi23_frac: float, K: float, d: float) -> Tuple[float, float]:
@@ -20,7 +20,7 @@ def ambiguity_resolution(phi12_frac: float, phi23_frac: float, K: float, d: floa
     Returns
     -------
     Tuple[float, float]
-        _description_
+        phi12_hat, phi23_hat (无模糊的1, 2阵元信号相位差, 无模糊的2, 3阵元信号相位差).
     """
     # 确保在[-1/2, 1/2)范围内
     phi12_frac, phi23_frac = np.mod(phi12_frac + 0.5, 1) - 0.5, np.mod(phi23_frac + 0.5, 1) - 0.5
@@ -42,30 +42,72 @@ def ambiguity_resolution(phi12_frac: float, phi23_frac: float, K: float, d: floa
     return n12 + phi12_frac, n23 + phi23_frac
 
 
-def far_locate(t12_e, t23_e, c, K, d):
-    """远场条件下的位置解算
+def far_locate(t12: float, t23: float, c: float, K: float, d: float) -> Tuple[float, float]:
+    """远场条件下的位置解算 (近似解). 方位角估计值对时延估计值容错相对精确解更高.
 
     Parameters
     ----------
-    t12_e : float
+    t12 : float
         tau12估计时延
-    t23_e : float
+    t23 : float
         tau23估计时延
+    c : float
+        声速 (m/s)
+    K : float
+        2, 3阵元间基线长度/1, 2阵元间基线长度
+    d : float
+        1, 2阵元间基线长度 (m)
 
     Returns
     -------
-    r_e : float
-        距离估计值
-    theta_e : float
-        方位角估计值
+    Tuple[float, float]
+        r, theta. 距离估计值, 方位角估计值
     """
-    t13_e = t12_e + t23_e
-    theta_e = np.arccos((c * t13_e) / ((K + 1) * d))
-    r_e = (
-        K * (K + 1) * ((d * np.sin(theta_e)) ** 2)
-        / (2 * c * (K * t12_e - t23_e))
+    t13 = t12 + t23
+    theta = np.arccos((c * t13) / ((K + 1) * d))
+    r = (
+        K * (K + 1) * ((d * np.sin(theta)) ** 2)
+        / (2 * c * (K * t12 - t23))
     )
-    return r_e, theta_e
+    return r, theta
+
+
+def near_locate(t12: float, t23: float, c: float, K: float, d: float) -> Tuple[float, float]:
+    """近场条件下的位置解算 (精确解).
+
+    Parameters
+    ----------
+    t12 : float
+        tau12估计时延
+    t23 : float
+        tau23估计时延
+    c : float
+        声速 (m/s)
+    K : float
+        2, 3阵元间基线长度/1, 2阵元间基线长度
+    d : float
+        1, 2阵元间基线长度 (m)
+
+    Returns
+    -------
+    Tuple[float, float]
+        r, theta (距离估计值, 方位角估计值).
+    """
+    theta2 = np.arccos(
+        (c * d**2 * (K**2 * t12 + t23) - c**3 * t12 * t23 * (t12 + t23))
+        / (K * (K + 1) * d**3 - c**2 * d * (K * t12**2 + t23**2))
+    )
+    r2 = (
+        (K * (K + 1) * d**2 - c**2 * (K * t12**2 + t23**2))
+        / (2 * c * (K * t12 - t23))
+    )
+    d2 = (K - 1) * d / 2
+    r = np.sqrt(r2**2 + d2**2 + 2 * r2 * d2 * np.cos(theta2))
+    if d2 == 0:
+        theta = theta2
+    else:
+        theta = np.arccos((r**2 + d2**2 - r2**2) / (2 * r * d2))
+    return r, theta
 
 
 def _time_delay_estimation_xcorr(x1: np.ndarray, x2: np.ndarray, f: float, fs: float) -> float:
@@ -103,9 +145,9 @@ def _time_delay_estimation_cpsd(x1: np.ndarray, x2: np.ndarray, f: float, fs: fl
         信号1序列 (左侧)
     x2 : np.ndarray
         信号2序列 (右侧)
-    f : int
+    f : float
         声源频率 (Hz)
-    fs : int
+    fs : float
         信号采样频率 (Hz)
 
     Returns
@@ -148,3 +190,36 @@ def time_delay_estimation(x1: np.ndarray, x2: np.ndarray, f: float, fs: float, m
         'xcorr': _time_delay_estimation_xcorr
     }
     return methods[method](x1, x2, f, fs)
+
+
+def chirp_delay_estimation(x1: np.ndarray, x2: np.ndarray, fs: float, f: float, period: float, T_on: float, up_sample_scale: int) -> float:
+    """基于chirp信号的时延估计
+
+    Parameters
+    ----------
+    x1 : np.ndarray
+        信号1序列 (左侧)
+    x2 : np.ndarray
+        信号2序列 (右侧)
+    fs : float
+        信号采样频率 (Hz)
+    f : float
+        chirp信号载波频率 (Hz)
+    period : float
+        chirp信号周期 (s)
+    T_on : float
+        chirp信号有效时长 (s)
+
+    Returns
+    -------
+    float
+        时延估计值tau (s)
+        """
+    t = np.arange(len(x1)) / fs
+    t_new = np.arange(up_sample_scale * len(x1)) / fs / up_sample_scale
+    x1 = interpolate.interp1d(t, x1, kind='cubic', fill_value='extrapolate')(t_new)  # type: ignore
+    x2 = interpolate.interp1d(t, x2, kind='cubic', fill_value='extrapolate')(t_new)  # type: ignore
+    s_ref = np.cos(2 * np.pi * f * t_new) * (t_new % period < T_on)
+    tau1_frac = _time_delay_estimation_xcorr(x1, s_ref, f, fs * up_sample_scale)
+    tau2_frac = _time_delay_estimation_xcorr(x2, s_ref, f, fs * up_sample_scale)
+    return tau1_frac - tau2_frac
