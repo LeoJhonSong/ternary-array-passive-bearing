3a490e40090ba4e0bca99428c80394c1981f929c
autodl-container-c2cc4fa1cc-67f868ff <LeoJhon.Song@outlook.com>
2024-05-22 15:10:18 +0800

    增加filter层, 改进可视化代码

--- feature_modules.py
+++ feature_modules.py
@@ -7,9 +7,11 @@ class Spectrogram(nn.Module):
     """band-limited spectrogram,
     accept waveform segments as input, shape: (batch_size, seconds, channels, time)
     """
-    def __init__(self, fs, nfft):
+
+    def __init__(self, fs, nfft, hop_factor):
         super().__init__()
         self.nfft = nfft
+        self.hop_factor = hop_factor
         f_low = 20e3
         f_high = 60e3
         f = np.linspace(0, fs / 2, int(nfft / 2 + 1))
@@ -22,7 +24,7 @@ class Spectrogram(nn.Module):
         spectrogram = torch.stft(
             x.view(-1, t_len),   # 压平batch与时间窗, 并行计算
             n_fft=self.nfft,
-            hop_length=int(self.nfft / 4),
+            hop_length=int(self.nfft / self.hop_factor),
             window=torch.hann_window(self.nfft, device=x.device),
             onesided=True,
             return_complex=True
@@ -33,10 +35,48 @@ class Spectrogram(nn.Module):
         return spectrogram
 
 
+class Filter(nn.Module):
+    def __init__(self, fs, nfft, fc):
+        super().__init__()
+        f_low = 20e3
+        f_high = 60e3
+        f = np.linspace(0, fs / 2, int(nfft / 2 + 1))
+        self.f = f[(f > f_low) & (f < f_high)]
+        self.fc_index = int(np.argwhere(self.f == fc)[0][0])
+
+    def forward(self, x):
+        spectrogram_batch = x.view(-1, x.shape[2], x.shape[3], x.shape[4])  # shape: (batch_size * seconds, channels, freq_limited, time)
+        magnitude_batch = torch.sum(torch.abs(spectrogram_batch), dim=1)  # shape: (batch_size * seconds, freq_limited, time)
+        mid_batch = torch.argmax(magnitude_batch[:, 1084, :], dim=1)  # find the time index of the maximum magnitude at the center frequency
+        pulse_mag_batch = torch.zeros(magnitude_batch.shape[:2])  # shape: (batch_size * seconds, freq_limited)
+        filtered_spectrogram_batch = torch.zeros_like(spectrogram_batch)
+        for i, mid in enumerate(mid_batch):
+            mid = int(mid)
+            pulse_mag_batch[i] = torch.sum(magnitude_batch[i, :, max(0, mid - 8):mid + 8], dim=1)
+            noise_mag = torch.mean(magnitude_batch[i, (self.f < 40e3) | (self.f > 44e3)])
+            sig_mag = pulse_mag_batch[i, self.fc_index]
+            snr = 10 * torch.log10((sig_mag - noise_mag)**2 / noise_mag**2)
+            half_band = max(int((12.5 * (snr.cpu() - 20) + 27)), 8)
+            filtered_spectrogram_batch[
+                :,
+                :,
+                self.fc_index - half_band:self.fc_index + half_band,
+                max(0, mid - 8):min(mid + 8, filtered_spectrogram_batch.shape[-1])
+            ] = spectrogram_batch[
+                :,
+                :,
+                1084 - half_band:1084 + half_band,
+                max(0, mid - 8):min(mid + 8, filtered_spectrogram_batch.shape[-1])
+            ]
+        filtered_spectrogram_batch = filtered_spectrogram_batch.reshape(x.shape)
+        return filtered_spectrogram_batch
+
+
 class CPSD_Phase_Spectrogram(nn.Module):
     """three element band-limited phase spectrogram of short time cross power spectral density (CPSD),
     accept spectrogram segments as input, shape: (batch_size, seconds, channels, freq_limited, time)
     """
+
     def __init__(self):
         super().__init__()
 
